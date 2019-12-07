480c1764a5e34c9e3982e8ae0c9e7bb84eee75d6
GThiago <gabriel.thiagohs@gmail.com>
2019-12-07 14:52:29 -0300

    maça

--- snake.py
+++ snake.py
@@ -2,6 +2,14 @@ import random
 import pygame
 from pygame.locals import *
 
+def on_grid_random():
+    x = random.randint(0,590)
+    y = random.randint(0,590)
+    return (x//10 * 10, y//10 * 10)         #gera numero multiplo de 10 pro grid
+
+def collision(c1,c2):
+    return(c1[0] ==c2[0]) and (c1[1]==c2[1])
+
 UP = 0
 RIGHT = 1
 DOWN = 2
@@ -15,7 +23,7 @@ snake = [(200,200),(210,200),(220,200)]
 snake_skin = pygame.Surface((10,10))
 snake_skin.fill((255,255,255))
 
-apple_pos = (random.randint(0,590),random.randint(0,5))
+apple_pos = on_grid_random()
 apple = pygame.Surface((10,10))
 apple.fill((255,0,0))
 
@@ -39,6 +47,10 @@ while True:
                 my_direction = LEFT
             if event.key == K_RIGHT:
                 my_direction = RIGHT
+
+    if collision(snake[0],apple_pos):
+        apple_pos = on_grid_random()
+        snake.append((0,0))                 #posicao na calda anterior
 
     for i in range(len(snake) -1,0,-1):
         snake[i] = (snake[i-1][0],snake[i-1][1])
