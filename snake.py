520c214ff6d586f602698b5b4c3389f9f45a84fb
GThiago <gabriel.thiagohs@gmail.com>
2019-12-08 11:52:30 -0300

    finish

--- snake.py
+++ snake.py
@@ -27,12 +27,21 @@ apple_pos = on_grid_random()
 apple = pygame.Surface((10,10))
 apple.fill((255,0,0))
 
+barraUp = [(0,00),(50,0),(100,0)]
+barraUp_skin = pygame.Surface((20,20))
+barraUp_skin.fill((250,250,200))
+
 my_direction = LEFT
 
 clock = pygame.time.Clock()
+valor = 10
 
-while True:
-    clock.tick(8)
+font = pygame.font.Font('freesansbold.ttf', 18)
+score = 0
+
+game_over = False
+while not game_over:
+    clock.tick(valor)
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -51,6 +60,25 @@ while True:
     if collision(snake[0],apple_pos):
         apple_pos = on_grid_random()
         snake.append((0,0))                 #posicao na calda anterior
+        if valor < 13:
+            valor+=1
+        score = score + 1
+
+    #check collided with boundaries
+    if snake[0][0] == 600 or snake[0][1] == 600 or snake[0][0] < 0 or snake[0][1] < 0:
+        game_over = True
+        break
+
+    #check if the snake has hit itself
+    #for i in range(1,len(snake)-1):
+    #    if snake[0][0] == snake[i][0] and snake[0][1] == snake[i][0]:
+    #        pygame.quit()
+    #        exit()
+    #        game_over= True
+    #        break
+
+    if game_over:
+        break
 
     for i in range(len(snake) -1,0,-1):
         snake[i] = (snake[i-1][0],snake[i-1][1])
@@ -67,7 +95,31 @@ while True:
 
     screen.fill((0,0,0))
     screen.blit(apple,apple_pos)
+
+    for x in range(0,600,10):
+        pygame.draw.line(screen, (40,40,40),(x,0),(x,600))
+    for y in range(0,600,10):
+        pygame.draw.line(screen,(40,40,40),(0,y),(600,y))
+
+    score_font = font.render('SCORE: %s' %(score), True, (255,255,255))
+    score_rect = score_font.get_rect()
+    score_rect.topleft = (600 - 120, 10)
+    screen.blit(score_font,score_rect)
+
     for pos in snake:
         screen.blit(snake_skin,pos)
 
     pygame.display.update()
+while True:
+    game_over_font = pygame.font.Font('freesansbold.ttf',75)
+    game_over_screen = game_over_font.render('Game Over!',True,(255,255,255))
+    game_over_rect = game_over_screen.get_rect()
+    game_over_rect.midtop = (600/2, 10)
+    screen.blit(game_over_screen,game_over_rect)
+    pygame.display.update()
+    pygame.time.wait(500)
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                exit()
